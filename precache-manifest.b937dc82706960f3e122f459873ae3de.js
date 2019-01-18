5f791763a09f4a4cd82ca38ffc1c6c11636a02f5
PMack <prestonmack@gmail.com>
2019-01-18 13:22:51 -0500

    Updates

---
+++ precache-manifest.b937dc82706960f3e122f459873ae3de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "001524410416434b8bfa",
+    "url": "/pmack99/19_Clicky/static/js/runtime~main.00152441.js"
+  },
+  {
+    "revision": "1bd58a8530c40a11ccc3",
+    "url": "/pmack99/19_Clicky/static/js/main.1bd58a85.chunk.js"
+  },
+  {
+    "revision": "fb9f72c6d9bfc22bcecb",
+    "url": "/pmack99/19_Clicky/static/js/1.fb9f72c6.chunk.js"
+  },
+  {
+    "revision": "1bd58a8530c40a11ccc3",
+    "url": "/pmack99/19_Clicky/static/css/main.a396114e.chunk.css"
+  },
+  {
+    "revision": "4cca341d387ad45dd55b0712428f9f19",
+    "url": "/pmack99/19_Clicky/index.html"
+  }
+];
